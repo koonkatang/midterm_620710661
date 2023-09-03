dbd479442f8a3112b9d360ae4362731d78f819de
koonkatang <koonkatang007@gmail.com>
2023-09-03 08:37:15 +0700

    midtermtest

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 
 // TODO: ใส่รหัสนักศึกษาที่ค่าสตริงนี้
-const studentId = 'Student ID';
+const studentId = '620710661';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
